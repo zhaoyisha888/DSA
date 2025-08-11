3185b205f08ed9aa6cd62494b9daf4abc361281d
yishazhao <zys15755433198@126.com>
2025-08-11 17:30:17 +0800

    新增图形左下三角

---
+++ GraphicalOutput/leftdownTriangle/leftdownTriangle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    // Êä³öÈý½ÇÐÎ
+    int num = 1;
+    for (int row = 0; row < n; row++) {           
+        for (int col = 0; col <= row; col++) {
+                printf("%02d", num);
+                num++;
+            } 
+            printf("\n");  
+        }
+        
+        return 0;
+}
+        
+    
