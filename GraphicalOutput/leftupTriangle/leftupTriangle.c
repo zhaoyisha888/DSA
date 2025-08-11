8b7fa876dbe8290483bb7a11fd318f241c4388c0
yishazhao <zys15755433198@126.com>
2025-08-11 17:36:50 +0800

    新增图形左上三角

---
+++ GraphicalOutput/leftupTriangle/leftupTriangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    // Êä³öÈý½ÇÐÎ
+    int num = 1;
+    for (int row = 0; row < n; row++) {           
+        for (int col = 0; col <= n-1-row; col++) {
+                printf("%02d", num);
+                num++;
+            } 
+            printf("\n");  
+        }
+        
+        return 0;
+}
+     
